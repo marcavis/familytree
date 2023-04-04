dfebe2aa52e8ac7c84f9b8d7840c2bd6da4212d8
marcavis <marcavis@yahoo.com.br>
2023-04-04 02:16:37 -0300

    v1.0

--- maketable.py
+++ maketable.py
@@ -22,7 +22,9 @@ for filename in os.listdir(folder_path):
 with open("families.txt", "r") as f:
     # Initialize the list of families
     families = []
-    
+    moms = []
+    dads = []
+
     # Loop over each line in the file
     for line in f:
         # Split the line into a tuple of names
@@ -31,6 +33,12 @@ with open("families.txt", "r") as f:
         # Append the tuple to the list of families
         families.append(thesenames)
 
+        moms.append(thesenames[1])
+        dads.append(thesenames[2])
+
+moms = list(set(moms))
+dads = list(set(dads))
+
 # Print the list of families
 # print(families)
 
@@ -38,8 +46,8 @@ names = list(set([elem for tup in families for elem in tup]))
 #names = ['lea', 'kazue', 'karin', 'aedus', 'aoife', 'myou']
 
 # Create a new image for the table
-table_width = len(names) * OC_SIZE + OC_SIZE
-table_height = len(names) * OC_SIZE + OC_SIZE
+table_width = len(dads) * OC_SIZE + OC_SIZE
+table_height = len(moms) * OC_SIZE + OC_SIZE
 table_image = Image.new('RGB', (table_width, table_height), (255, 255, 255))
 
 
@@ -59,8 +67,8 @@ draw.text((20,0), "DADS→", font=font, fill=(127,0,0))
 
 # Draw the images or names onto the table
 
-rollcall = enumerate(names)
-for i, name in rollcall:
+rollcalldads = enumerate(dads)
+for i, name in rollcalldads:
     x = (i + 1) * grid_step 
     y = 0 * grid_step 
     try:
@@ -68,6 +76,11 @@ for i, name in rollcall:
     except KeyError:
         # If the image doesn't exist, draw the name instead
         draw.text((x, y), name, font=font, fill=(0, 0, 0))
+
+rollcallmoms = enumerate(moms)
+for i, name in rollcallmoms:
+    x = (i + 1) * grid_step 
+    y = 0 * grid_step 
     try:
         table_image.paste(images[name], (y, x), images[name])
     except KeyError:
@@ -75,15 +88,14 @@ for i, name in rollcall:
         draw.text((y, x), name, font=font, fill=(0, 0, 0))
 
 
-
-
 for p in families:
-    childpos = (names.index(p[2]) * grid_step + grid_step, names.index(p[1]) * grid_step + grid_step)
+    childpos = (dads.index(p[2]) * grid_step + grid_step, moms.index(p[1]) * grid_step + grid_step)
     try:
         table_image.paste(images[p[0]], childpos, images[p[0]])
     except KeyError:
         # If the image doesn't exist, draw the name instead
-        draw.text(childpos, p[0], font=font, fill=(0, 0, 0))
+        table_image.paste(images['unknown'], childpos, images['unknown'])
+        draw.text((childpos[0]+20, childpos[1]+100), p[0], font=font, fill=(0, 0, 0))
 
 # Save the table image to a file
 table_image.save('board.png')
