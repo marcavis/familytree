4a859a33a4618b289b8059d2f567d3d8644f9f28
marcavis <marcavis@yahoo.com.br>
2023-04-08 01:58:26 -0300

    still sorta working

--- newtree.py
+++ newtree.py
@@ -5,6 +5,7 @@ import os
 folder_path = 'portraits'
 images = {}
 OC_SIZE = 150
+SPACING = 20
 
 class Person:
     def __init__(self, name, mom=None, dad=None):
@@ -57,9 +58,9 @@ with open("families.txt", "r") as f:
         people[fam[0]].mom = people[fam[1]]
         people[fam[0]].dad = people[fam[2]]
         if fam[2] not in people[fam[1]].sires:
-            people[fam[1]].sires.append(fam[2])
+            people[fam[1]].sires.append((fam[2], fam[0], max(people[fam[1]].getlevel(), people[fam[2]].getlevel())))
         if fam[1] not in people[fam[2]].dams:
-            people[fam[2]].dams.append(fam[1])
+            people[fam[2]].dams.append((fam[1], fam[0], max(people[fam[1]].getlevel(), people[fam[2]].getlevel())))
 
 
 print(people["aoife"].dams)
@@ -110,26 +111,59 @@ maxlevel = max(levels) + 1
 maxheight = max([levels.count(x) for x in range(maxlevel)])
 
 # Create a new image for the table
-table_width = maxlevel * OC_SIZE * 3
-table_height = maxheight * OC_SIZE * 2
+table_width = maxlevel * (OC_SIZE + SPACING) * 2
+table_height = maxheight * (OC_SIZE + SPACING) * 2
 table_image = Image.new('RGB', (table_width, table_height), (255, 255, 255))
 draw = ImageDraw.Draw(table_image)
 
+
+births = [people[p].sires for p in people]
+#let's start with moms
 for i in range(0, maxlevel):
     j = 0
     for p in people:
         person = people[p]
-        if person.getlevel() == i:
-            try:
-                table_image.paste(images[person.name], (i*OC_SIZE, j*OC_SIZE), images[person.name])
-            except KeyError:
-                # If the image doesn't exist, draw the name instead
-                table_image.paste(images['unknown'], (i*OC_SIZE, j*OC_SIZE), images['unknown'])
-                draw.text((i*OC_SIZE, j*OC_SIZE), person.name, font=font, fill=(0, 0, 0))
-            except ValueError:
-                #this portrait isn't transparent
-                table_image.paste(images[person.name], (i*OC_SIZE, j*OC_SIZE))
-            j = j + 1
+        if person.getlevel() == i and len(person.sires) > 0:
+            for other in person.sires:
+                dad = people[other[0]]
+                kid = people[other[1]]
+                mompos = (i*OC_SIZE*2 + i*SPACING, j*OC_SIZE + j*SPACING)
+                dadpos = (i*OC_SIZE*2 + i*SPACING, j*OC_SIZE + OC_SIZE + j*SPACING)
+                kidpos = (i*OC_SIZE*2 + OC_SIZE + i*SPACING, j*OC_SIZE + OC_SIZE//2 + j*SPACING)
+
+                for box in [mompos, dadpos, kidpos]:
+                    draw.rectangle((box[0], box[1], box[0] + OC_SIZE - 1, box[1] + OC_SIZE - 1), outline="black", width=2)
+                
+                try:
+                    table_image.paste(images[person.name], mompos, images[person.name])
+                except KeyError:
+                    # If the image doesn't exist, draw the name instead
+                    table_image.paste(images['unknown'], mompos, images['unknown'])
+                    draw.text(mompos, person.name, font=font, fill=(0, 0, 0))
+                except ValueError:
+                    #this portrait isn't transparent
+                    table_image.paste(images[person.name], mompos)
+
+                try:
+                    table_image.paste(images[dad.name], dadpos, images[dad.name])
+                except KeyError:
+                    # If the image doesn't exist, draw the name instead
+                    table_image.paste(images['unknown'], dadpos, images['unknown'])
+                    draw.text(dadpos, dad.name, font=font, fill=(0, 0, 0))
+                except ValueError:
+                    #this portrait isn't transparent
+                    table_image.paste(images[dad.name], dadpos)
+
+                try:
+                    table_image.paste(images[kid.name], kidpos, images[kid.name])
+                except KeyError:
+                    # If the image doesn't exist, draw the name instead
+                    table_image.paste(images['unknown'], kidpos, images['unknown'])
+                    draw.text(kidpos, kid.name, font=font, fill=(0, 0, 0))
+                except ValueError:
+                    #this portrait isn't transparent
+                    table_image.paste(images[kid.name], kidpos)
+                j = j + 2
 
 
 # Save the table image to a file
